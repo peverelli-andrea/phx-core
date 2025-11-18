e1964984a369327a72d3ed77bf1d7f7ef0ea7a32
Peverelli Andrea <ndrpeverelli@gmail.com>
2025-11-18 22:55:53 +0100

    quick fix

--- src/ForegroundColor.php
+++ src/ForegroundColor.php
@@ -5,7 +5,7 @@ namespace Phx\Core;
 enum ForegroundColor: string
 {
 	case ON_SURFACE = "on_surface";
-	case ON_SURFACE_VARIANT = "on_surface_container";
+	case ON_SURFACE_VARIANT = "on_surface_variant";
 	case INVERSE_ON_SURFACE = "inverse_on_surface";
 	case ON_SURFACE_CONTAINER = "on_surface_container";
 	case ON_PRIMARY = "on_primary";
